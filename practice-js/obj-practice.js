d2f3d8e85c6fbca19bb47f7f1da0f5b80cd30896
G184712021 <218471@st.takushoku-u.ac.jp>
2023-05-11 15:17:12 +0900

    asd

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,8 +16,8 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 console.log(campus.address);
-for(let n of buildingD){
-	console.log(buildingD[n]);
+for(let n of campus,buildingD){
+	console.log(campus,buildingD[n]);
 }
 for(let t of gakka){
 	console.log(gakka[t]);
