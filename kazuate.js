bc5be21aa71d7806b3a24ae5647d9ea0b0dcdf68
G184712021 <218471@st.takushoku-u.ac.jp>
2023-06-01 16:03:15 +0900

    as

--- kazuate.js
+++ kazuate.js
@@ -13,8 +13,8 @@ b1.addEventListener('click', hantei);
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   let i = document.querySelector('#nyuryoku');
-  i.textContent = i;
-  let yoso = Number(i);
+  i.textContent = Number(i);
+  let yoso = i;
   //i.value;
   //t = parseInt(t, 10);
   kaisu++;
