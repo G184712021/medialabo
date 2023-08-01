86b1b0cdbd8528c50b0cb25e75ecdd891d1fc6fc
G184712021 <218471@st.takushoku-u.ac.jp>
2023-08-01 15:22:11 +0900

    ss

--- MBAproject.js
+++ MBAproject.js
@@ -1,8 +1,3 @@
-// 答え
-
-//console.log('答え（デバッグ用）: ' + kotae);
-
-// 入力回数（予想回数）
 let kaisu = 0;
 let syouri = 0;
 let haiboku = 0;
@@ -13,7 +8,7 @@ b1.addEventListener('click', hantei);
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   let kotae = Math.floor(Math.random()*61) + 1;
-  let i = document.querySelector('#nyuryoku');
+  let i = document.querySelector('#nyuryoku[name="suuzi"]');
   let yoso = i.value;
   yoso = Number(yoso);
   kaisu++;
@@ -45,24 +40,3 @@ function hantei() {
     }
   }
 }
-
-
-
-
-
-  /*if (kaisu >= 6) {
-    kekka.textContent = "すでにゲームは終わっています";
-  } else {
-    if (kaisu === 5 && syouri < haiboku) {
-      kekka.textContent = "残念!!不合格です．";
-    } else if (kaisu === 5 && syouri > haiboku) {
-      kekka.textContent = "おめでとう!!合格です.";
-    } else if(yoso > tekianswer){
-      kekka.textContent = "あなたの勝ちです!いいですね!";
-      syouri++;
-    } else {
-      kekka.textContent = "あなたの負けです。頑張りましょう.";
-      haiboku++;
-    }
-  }
-}*/
